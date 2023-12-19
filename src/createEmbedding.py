91e3b769550130f52f013e7740fd14e137bebf48
gregoriofsg <dasgregorio@gmail.com>
2023-12-19 19:20:02 -0300

    Update createEmbedding.py

--- src/createEmbedding.py
+++ src/createEmbedding.py
@@ -9,11 +9,13 @@ import time
 import csv
 import argparse
 import sys
+import string
 
 verbose = False
 
 
 def select_important(doc,mark):
+    doc = doc.lower()
     match = re.search(fr' {mark}[^\n]*',doc)
     if match:
         start = match.start()
@@ -22,24 +24,30 @@ def select_important(doc,mark):
         final_doc = doc
     return final_doc    
         #Palavras irrelevantes
-
     
 def clean_text(text):
+    stop_words = []
+    match_stops = []
+    match_patterns = []
+    #print("###############################TEXTO ORIGINAL ######################3#")
+    #print(text)
+    text_cleaned = text.lower()
+    stop_words_nltk = set(stopwords.words('portuguese'))
+    stop_words.append(list(stop_words_nltk))
+    stop_words.append(["nº","cep","telefone","rua","avenida","endereço","fax","fones"])
+    stop_words.append(["egrégia","egrégio","eg","e.g."])
+    stop_words.append(["copy","reg","trade","ldquo","rdquo","lsquo","rsquo","bull","middot","sdot","ndash","mdash","cent","pound"])
+    stop_words.append(["euro","ne","frac12","frac14","frac34","deg","larr","rarr","uarr","darr","egrave","eacute","ccedil","hellip"])
+    match_pattern = [r'\b_+(?:\d+|[a-zA-Z]+)?\b',r'https?://\S+',r'www\.\S+',r'\S+@\S+',r'^\d{3}.\d{3}.\d{3}-\d{2}$',r'^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$',r'\d{2}/\d{2}/\d{4}[ ,]',r'procuradoria regional (federal|da união) da \d+[ªa] região',r'tribunal regional federal( da)? \d+[ªa] região',r'advocacia( -)?geral da união',r'(excelentíssimo|senhor|vice-presidente|desembargador|\(a\))',r'procuradoria( -)?geral federal',r'escritório de advocacia',r'(superior )?tribunal de justiça',r'supremo tribunal federal',r'fones',r'fax',r'tribunal regional federal']
 
-    stop_words = set(stopwords.words('portuguese'))
-    stop_words.update(["nº","cep","telefone","rua","avenida","endereço","fax","fones"])
-    stop_words.update(["egrégia","egrégio","eg","e.g."])
-    stop_words.update(["copy","reg","trade","ldquo","rdquo","lsquo","rsquo","bull","middot","sdot","ndash","mdash","cent","pound"])
-    stop_words.update(["euro","ne","frac12","frac14","frac34","deg","larr","rarr","uarr","darr","egrave","eacute","ccedil","hellip"])
-    
-    tokens = nltk.word_tokenize(text, language='portuguese')
-    tokens_cleaned = [token for token in tokens if token not in stop_words]
-    text_cleaned = ' '.join(tokens_cleaned)    
-    match_pattern = [r'\b_+(?:\d+|[a-zA-Z]+)?\b',r'https?://\S+',r'www\.\S+',r'\S+@\S+',r'^\d{3}.\d{3}.\d{3}-\d{2}$',r'^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$',r'\d{2}/\d{2}/\d{4}[ ,]',r'procuradoria regional (federal|da união) da \d+[ªa] região',r'tribunal regional federal[ da] \d+[ªa] região',r'advocacia[ -]geral da união',r'(excelentíssimo|senhor|vice-presidente|desembargador|\(a\))',r'procuradoria[ -]geral federal',r'escritório de advocacia',r'[ superior] tribunal de justiça',r'supremo tribunal federal',r'fones',r'fax']
-    subs = [''] * len(match_pattern)
-
+    stop_words_flat = [word for sublist in stop_words for word in sublist]
+    match_pattern.extend([rf'\b{re.escape(palavra)}\b' for palavra in stop_words_flat])
+    subs = [''] * len(match_stops)
     for match_pattern, subs in zip(match_pattern,subs):
+        #print(match_pattern)
         text_cleaned = re.sub(match_pattern,subs,text_cleaned)
+    #print("################################TEXTO LIMPO #################################")
+    #print(text_cleaned)
     return text_cleaned
     
 def process_corpus(path,clean,begin,column):
@@ -49,7 +57,7 @@ def process_corpus(path,clean,begin,column):
         docs = []
         num_cadastrado = []
         indice = []
-        
+        text = ""
         print("############### Lendo registros do corpus ###############")
         for i, linha in resp.iterrows():
             #Exibe percentual concluido
@@ -64,17 +72,15 @@ def process_corpus(path,clean,begin,column):
                     #Se houver um marco de trecho relevante do texto
                     if begin:
                         text = select_important(text,begin)
-                    
+                        #print(text)
                     #Se tiver de limpar o texto
                     if clean:
                         text_cleaned = clean_text(text)
                         num_cadastrado.append(int(linha['num_tema_cadastrado']))
+                        #print(text_cleaned)
                         docs.append(text_cleaned)
                         indice.append(i)     
                     else:
-                        sys.stdout.write("\r ")  # \r faz o cursor retroceder ao início da linha
-                        sys.stdout.write(f' Percentual concluído: {i/size*100:.2f}%')
-                        sys.stdout.flush()  # Força a impressão imediata
                         num_cadastrado.append(int(linha['num_tema_cadastrado']))
                         docs.append(text)
                         indice.append(i)
