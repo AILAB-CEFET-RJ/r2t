1ee78e8cd632cc63df55c8b18ad2360f8581ea90
gregoriofsg <dasgregorio@gmail.com>
2024-01-02 23:38:02 -0300

    Update createEmbedding.py

--- src/createEmbedding.py
+++ src/createEmbedding.py
@@ -1,5 +1,7 @@
 import nltk
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 import re
 from sentence_transformers import SentenceTransformer, util
 import pickle
@@ -10,6 +12,8 @@ import csv
 import argparse
 import sys
 import string
+import torch
+
 
 verbose = False
 
@@ -24,31 +28,38 @@ def select_important(doc,mark):
         final_doc = doc
     return final_doc    
         #Palavras irrelevantes
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    text_without_punctuation = text.translate(translator)
+    return text_without_punctuation
     
-def clean_text(text):
-    stop_words = []
-    match_stops = []
-    match_patterns = []
-    #print("###############################TEXTO ORIGINAL ######################3#")
-    #print(text)
-    text_cleaned = text.lower()
-    stop_words_nltk = set(stopwords.words('portuguese'))
-    stop_words.append(list(stop_words_nltk))
-    stop_words.append(["nº","cep","telefone","rua","avenida","endereço","fax","fones"])
-    stop_words.append(["egrégia","egrégio","eg","e.g."])
-    stop_words.append(["copy","reg","trade","ldquo","rdquo","lsquo","rsquo","bull","middot","sdot","ndash","mdash","cent","pound"])
-    stop_words.append(["euro","ne","frac12","frac14","frac34","deg","larr","rarr","uarr","darr","egrave","eacute","ccedil","hellip"])
-    match_pattern = [r'\b_+(?:\d+|[a-zA-Z]+)?\b',r'https?://\S+',r'www\.\S+',r'\S+@\S+',r'^\d{3}.\d{3}.\d{3}-\d{2}$',r'^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$',r'\d{2}/\d{2}/\d{4}[ ,]',r'procuradoria regional (federal|da união) da \d+[ªa] região',r'tribunal regional federal( da)? \d+[ªa] região',r'advocacia( -)?geral da união',r'(excelentíssimo|senhor|vice-presidente|desembargador|\(a\))',r'procuradoria( -)?geral federal',r'escritório de advocacia',r'(superior )?tribunal de justiça',r'supremo tribunal federal',r'fones',r'fax',r'tribunal regional federal']
-
-    stop_words_flat = [word for sublist in stop_words for word in sublist]
-    match_pattern.extend([rf'\b{re.escape(palavra)}\b' for palavra in stop_words_flat])
-    subs = [''] * len(match_stops)
-    for match_pattern, subs in zip(match_pattern,subs):
-        #print(match_pattern)
-        text_cleaned = re.sub(match_pattern,subs,text_cleaned)
-    #print("################################TEXTO LIMPO #################################")
-    #print(text_cleaned)
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('portuguese'))
+    stop_words.update(["nº","cep","telefone","rua","avenida","endereço","fax","fones"])
+    stop_words.update(["egrégia","egrégio","eg","e.g."])                   
+    
+    stop_words.update(["copy","reg","trade","ldquo","rdquo","lsquo","rsquo","bull","middot","sdot","ndash"])  
+    stop_words.update(["mdash","cent","pound","euro","ne","frac12","frac14","frac34","deg","larr","rarr","uarr","darr","egrave","eacute","ccedil","hellip"])
+    # Tokenizando o texto
+    tokens = word_tokenize(text, language='portuguese')
+    tokens_cleaned = [token for token in tokens if token not in stop_words]
+    detokenizer = TreebankWordDetokenizer()
+    detokenized_text = detokenizer.detokenize(tokens_cleaned)
+    text_cleaned = ' '.join(tokens_cleaned)
     return text_cleaned
+
+    
+def clean_text(doc):
+    final_doc = doc
+    #Padrões irrelevantes
+    match_pattern = [r',\s*,',r'\bpágina\s+(\d+)\s+(\d+)\b',r'\bpágina\s+(\d+)\s+de\s+(\d+)\b',r'\?',r'\b_+(?:\d+|[a-zA-Z]+)?\b',r'https?://\S+',r'www\.\S+',r'\S+@\S+',r'^\d{3}.\d{3}.\d{3}-\d{2}$',r'^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$',r'\d{2}/\d{2}/\d{4}[ ,]',r'\bprocuradoria regional (federal|da união) da \d+[ªa] região\b',r'\btribunal regional federal( da) \d+[ªa] região\b',r'\badvocacia( -)geral da união\b',r'\b(excelentíssimo|senhor|vice-presidente|desembargador|\(a\))\b',r'\bprocuradoria[ -]geral federal\b',r'\bescritório de advocacia\b',r'\b(superior) tribunal de justiça\b',r'\bsupremo tribunal federal\b',r'\bfones\b',r'\bfax\b']
+    subs = [''] * len(match_pattern)
+    for match_pattern, subs in zip(match_pattern,subs):
+        final_doc = re.sub(match_pattern,subs,final_doc)
+    
+    final_doc = remove_stopwords(final_doc)
+    
+    return final_doc
     
 def process_corpus(path,clean,begin,column):
     try:
@@ -111,21 +122,44 @@ def createFileName(name,clean):
         return f'{name}_EMBEDDING_CLEAN.pkl'
     else:
         return f'{name}_EMBEDDING.pkl'
+
+def create_embedding(file,indice,corpus,num,model,v,c,data_type):
     
-def create_embedding(file,indice,corpus,num,model,v,c):
     sentence_model = SentenceTransformer(model)
     nameEmbedding = createFileName(file.name.split('.')[0],c)
-    #verbose
-    if v:
-        corpus_embedding = sentence_model.encode(corpus,show_progress_bar=True)
+    if(data_type == 'tema'):
+        print("############### Gerando embeddings dos temas ###############")
+        i = 0
+        for linha in corpus:
+            i = i+1
+             #Exibe percentual concluido
+            sys.stdout.write("\r ")  # \r faz o cursor retroceder ao início da linha
+            sys.stdout.write(f' Percentual concluído: {i/len(corpus)*100:.2f}%')
+            sys.stdout.flush()  # Força a impressão imediata
+            tema_clean = remove_punctuation(linha)          
+            tokenized_temas = tema_clean.split(" ")
+        #verbose
+        if v:
+            corpus_embedding = sentence_model.encode(corpus,show_progress_bar=True)
+        else:
+            corpus_embedding = sentence_model.encode(corpus,show_progress_bar=False)
+
+        with open(nameEmbedding, "wb") as fOut:
+                pickle.dump({'indice':indice,'sentences': corpus,'numTema':num ,'embeddings': corpus_embedding}, fOut,protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"Embedding salvo no arquivo {nameEmbedding}")
     else:
-        corpus_embedding = sentence_model.encode(corpus,show_progress_bar=False)
-    
-    with open(nameEmbedding, "wb") as fOut:
-            pickle.dump({'indice':indice,'sentences': corpus,'numTema':num ,'embeddings': corpus_embedding}, fOut,protocol=pickle.HIGHEST_PROTOCOL)
-    print(f"Embedding salvo no arquivo {nameEmbedding}")   
+        #verbose
+        if v:
+            corpus_embedding = sentence_model.encode(corpus,show_progress_bar=True)
+        else:
+            corpus_embedding = sentence_model.encode(corpus,show_progress_bar=False)
+
+        with open(nameEmbedding, "wb") as fOut:
+                pickle.dump({'indice':indice,'sentences': corpus,'numTema':num ,'embeddings': corpus_embedding}, fOut,protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"Embedding salvo no arquivo {nameEmbedding}")
     
 def main(args):
+    conteudo = []
     print("############### PROGRAMA DE GERAÇÃO DE EMBEDDINGS ###############")
     print("############### Configuração ###############")
     print(f"Sentence-BERT Model : {args.model}")
@@ -140,9 +174,11 @@ def main(args):
     corpus = process_corpus(args.corpus_csv_file,args.clean,args.begin_point,args.column)
     
     #Se não houver uma coluna indice, cria uma
-    if 'indice' not in corpus.columns:
-        corpus['indice'] = range(1,len(corpus)+1)
-    create_embedding(args.corpus_csv_file,corpus['indice'],corpus[args.column],corpus['num_tema_cadastrado'],args.model,args.verbose,args.clean)
+    #if 'indice' not in corpus.columns:
+        #corpus['indice'] = range(1,len(corpus)+1)
+
+    conteudo = corpus[args.column].tolist()
+    create_embedding(args.corpus_csv_file,corpus['indice'],conteudo,corpus['num_tema_cadastrado'],args.model,args.verbose,args.clean, args.data_type)
     
     print("Salvando log ...")
     tempo_fim = time.time()
@@ -170,6 +206,7 @@ if __name__=="__main__":
     
     parser = argparse.ArgumentParser(description='Generate text embedding using Sentence-BERT model')
     parser.add_argument('corpus_csv_file', type=argparse.FileType('r'), help='File containing the corpus')
+    parser.add_argument('data_type',choices=['recurso','tema'],help='Indicates whether the data type is recurso or tema')
     parser.add_argument('column',help='Column that contains the text to be transformed into embedding')
     parser.add_argument('model',default='distiluse-base-multilingual-cased-v1',nargs='?', help='The Sentence-BERT model used to generate embedding : Default = distiluse-base-multilingual-cased-v1')
     parser.add_argument('--clean',action='store_true',help='Remove stopwords before creating embedding')
