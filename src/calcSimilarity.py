cf34b0f2bea9399a241023242d23b97140b3ccc3
gregoriofsg <dasgregorio@gmail.com>
2024-09-09 14:31:44 -0300

    Update calcSimilarity.py

--- src/calcSimilarity.py
+++ src/calcSimilarity.py
@@ -11,287 +11,233 @@ import sys
 from sentence_transformers import SentenceTransformer, util
 import torch
 
-# Instalação dos pacotes necessários
-subprocess.run(["pip", "install", "rank_bm25","numexpr"])
+# Install required packages
+subprocess.run(["pip", "install", "rank_bm25", "numexpr"])
 
 def remove_punctuation(text):
     translator = str.maketrans('', '', string.punctuation)
-    text_without_punctuation = text.translate(translator)
-    return text_without_punctuation
+    return text.translate(translator)
 
+def read_text(dataframe, row, column):
+    return dataframe.at[row, column]
 
-def read_text(dataframe, linha, coluna):
-    texto = dataframe.at[linha, coluna]
-    return texto
+def sort_list(lst):
+    return sorted(lst, key=lambda x: x[1], reverse=True)
 
-def sort_list(lista):
-    return(sorted(lista, key = lambda x: x[1],reverse=True))
+def create_bm25_similarity_list(themes, k, real_theme):
+    sorted_list = sort_list(themes)
+    real_theme_info = []
 
+    for i, item in enumerate(sorted_list):
+        if item[0] == real_theme:
+            real_theme_info.append(i + 1)  # Position of the real theme in the ranking
+            real_theme_info.append(item[1])  # Similarity of the real theme according to BM25
+            break
 
-def create_list_similarity_bm25(temas ,k,tema_real):
-  lista_similaridade = temas
-  lista_tema_real = []
+    ranking = sorted_list[:k]
+    return ranking, real_theme_info
 
-  sorted_list = sort_list(lista_similaridade)
+def calculate_cosine_similarity(topics, theme_embeddings, theme_numbers, k, real_theme):
+    similarity_list = []
+    real_theme_info = []
 
-  for i, linha in enumerate(sorted_list):
-      if(linha[0]==tema_real):
-        lista_tema_real.append(i+1)#identifica posição do tema real no ranking
-        lista_tema_real.append(linha[1])#similaridade do tema real segundo bm25
-        break
-  ranking = sorted_list[:k]
-  return ranking,lista_tema_real
+    for embedding, number in zip(theme_embeddings, theme_numbers):
+        query_embedding = topics
+        tensor_similarity = util.cos_sim(query_embedding, embedding)
+        similarity_value = tensor_similarity.item()
+        similarity_list.append((number, similarity_value))
 
-def calc_similarity_cosine(topics, temas_embeddings,temas_numTema ,k,tema_real):
-  lista_similaridade = []
-  lista_tema_real = []
-  
-  for temas_embeddings,temas_numTema in zip(temas_embeddings,temas_numTema):
-      query_embedding = topics
-      #print(f"Tema {indice} : {tupla_num_tema[0]}")
-      #tema_cleaned = clean_text(temas_embeddings)
-      tensor_similaridade = util.cos_sim(query_embedding, temas_embeddings)
-      valor_similaridade = tensor_similaridade.item()
-      tupla = (temas_numTema,valor_similaridade)
-      lista_similaridade.append(tupla)
+    sorted_list = sort_list(similarity_list)
+    for i, item in enumerate(sorted_list):
+        if item[0] == real_theme:
+            real_theme_info.append(i + 1)  # Position of the real theme in the ranking
+            real_theme_info.append(item[1])
+            break
 
-  sorted_list = sort_list(lista_similaridade)
-  for i, linha in enumerate(sorted_list):
-      if(linha[0]==tema_real):
-        lista_tema_real.append(i+1)#identifica posição do tema real no ranking
-        lista_tema_real.append(linha[1]) 
-        break
-  ranking = sorted_list[:k]
-  return ranking,lista_tema_real
+    ranking = sorted_list[:k]
+    return ranking, real_theme_info
 
-def createColumns(k):
-      #k refere-se ao numero de elementos no ranking
-    colunas = []
-    colunas.append("indice")
-    colunas.append("num_tema_cadastrado")
+def create_columns(k):
+    columns = ["index", "registered_theme_number"]
     for i in range(1, k + 1):
-        nome = f"sugerido_{i}"
-        colunas.append(nome)
-        nome = f"similaridade_{i}"
-        colunas.append(nome)
-    #Posicao que o tema cadastrado por analista ocupa no ranking
-    colunas.append("posicao_tema_real")
-    colunas.append("similaridade_tema_real")
-    return colunas
+        columns.append(f"suggested_{i}")
+        columns.append(f"similarity_{i}")
+    columns.append("real_theme_position")
+    columns.append("real_theme_similarity")
+    return columns
 
+def create_file(name, mode, rank):
+    name_parts = name.split('.')
+    new_file_name = f'CLASSIFIED_{name_parts[0]}_BM25.csv' if mode == 'B' else f'CLASSIFIED_{name_parts[0]}_COSINE.csv'
+    columns = create_columns(rank)
+    pd.DataFrame(columns=columns).to_csv(new_file_name, index=False)
+    return new_file_name
 
-def createFile(name,mode,rank):
-    n = name.split('.')
-    new=''
-    if (mode=='B'):
-        new =f'CLASSFIED_{n[0]}_BM25.csv'
-    if (mode=='C'):
-        new =f'CLASSFIED_{n[0]}_COSINE.csv'
-    file_columns = createColumns(rank)
-    df = pd.DataFrame(columns=file_columns)
-    df.to_csv(new,index=False)
-    return new
-    
-#Implementando padrao de projeto strategy
+# Strategy pattern implementation
 
-# Definindo interfaces (estratégias)
-class Estrategia(ABC):
+class SimilarityStrategy(ABC):
     @abstractmethod
-    def executar(self, corpus, temas, typeSim,verbose=None):
+    def execute(self, corpus, themes, similarity_type, rank, verbose=None):
         pass
 
-class EstrategiaBM25(Estrategia):
+class BM25Strategy(SimilarityStrategy):
     def __init__(self):
         self.bm25 = None
         
-    def calc_scores_bm25(self,topico):
+    def calculate_bm25_scores(self, topic):
         if self.bm25 is None:
             raise ValueError("BM25 not initialized.")
-        query = topico.replace("-","")
+        query = topic.replace("-", "")
         tokenized_query = query.split()
-        doc_scores = self.bm25.get_scores(tokenized_query)
-        return doc_scores
+        return self.bm25.get_scores(tokenized_query)
 
-    def executar(self, corpus,temas, typeSim,rank,verbose=None):
-        temas_data = pd.DataFrame()
+    def execute(self, corpus, themes, similarity_type, rank, verbose=None):
+        themes_data = pd.DataFrame()
         corpus_data = pd.DataFrame()
-        with open(corpus, "rb") as fIn:
-            data = pickle.load(fIn)
-            corpus_data['indice'] = data['indice']
+        
+        with open(corpus, "rb") as f:
+            data = pickle.load(f)
+            corpus_data['index'] = data['index']
             corpus_data['topics'] = data['topics']
-            #corpus_data['topicsEmbeddings']
-            corpus_data['numTema'] = data['numTema']
-        with open(temas, "rb") as fIn:
-            data = pickle.load(fIn)
-            #Columns
-            temas_data['indice'] = data['indice']
-            temas_data['sentences'] = data['sentences']
-            #temas_data['embeddings'] = data['embeddings']
-            temas_data['numTema']= data['numTema']
-        print("Calculando Similaridade com BM25")
+            corpus_data['theme_number'] = data['theme_number']
         
-        #Salva dados de similaridade em arquivo
-        file_similarity = createFile(corpus,typeSim,rank)
+        with open(themes, "rb") as f:
+            data = pickle.load(f)
+            themes_data['index'] = data['index']
+            themes_data['sentences'] = data['sentences']
+            themes_data['theme_number'] = data['theme_number']
+
+        print("Calculating similarity using BM25")
+
+        file_similarity = create_file(corpus, similarity_type, rank)
         
-        #Processamento do texto alvo
-        temas = []
-        for indice, linha in temas_data.iterrows():
-            tema_clean = remove_punctuation(linha['sentences'])
-            temas.append(tema_clean)           
-        tokenized_corpus = [doc.split(" ") for doc in temas]
+        cleaned_themes = [remove_punctuation(row['sentences']) for _, row in themes_data.iterrows()]
+        tokenized_corpus = [doc.split(" ") for doc in cleaned_themes]
         self.bm25 = BM25Okapi(tokenized_corpus)
 
-        
-        
-        #Processamento da consulta
-        for indice, linha in corpus_data.iterrows():
-            sys.stdout.write("\r ")  # \r faz o cursor retroceder ao início da linha
-            sys.stdout.write(f' Percentual concluído: {indice/len(corpus_data)*100:.2f}%')
-            sys.stdout.flush()  # Força a impressão imediata
+        for idx, row in corpus_data.iterrows():
+            sys.stdout.write("\r ")
+            sys.stdout.write(f' Progress: {idx/len(corpus_data)*100:.2f}%')
+            sys.stdout.flush()
             print(" ", end='\r')
-            doc_scores = self.calc_scores_bm25(linha['topics'])
-            temas_classificados = temas_data[['numTema']].copy()
-            temas_classificados['similaridade']= doc_scores
-            temas_classificados.columns = ['numTema','similaridade']
-            list_temas_classificados = list(temas_classificados.itertuples(index=False, name=None))
-            dados = []
-            dados.append(indice)
-              #numero do tema cadastrado por um analista
+            doc_scores = self.calculate_bm25_scores(row['topics'])
+            classified_themes = themes_data[['theme_number']].copy()
+            classified_themes['similarity'] = doc_scores
+            classified_themes.columns = ['theme_number', 'similarity']
+            sorted_themes = list(classified_themes.itertuples(index=False, name=None))
+            data_to_write = [idx, int(row['theme_number'])]
+
             try:
-                  #numeracao de tema cadastrado pelo analista para o recurso
-                dados.append(int(linha['numTema']))
-            except Exception as erro:
-                print(f"Erro ao capturar numero de tema cadastrado {indice}")
+                ranking, real_theme_info = create_bm25_similarity_list(sorted_themes, rank, row['theme_number'])
+            except Exception as e:
+                print(f"Error calculating similarity index {idx}")
                 continue
-            try:  
-                ranking , lista_tema_real = create_list_similarity_bm25( list_temas_classificados, rank, linha['numTema'])
-            except Exception as erro:
-                print(f"Erro calculo similaridade indice {indice}") 
-                continue
-                
 
-            for i, tupla_num_tema in enumerate(ranking):
-                #captura numero do tema sugerido e valor da similaridade
-                dados.append(tupla_num_tema[0])
-                dados.append(tupla_num_tema[1])
+            for item in ranking:
+                data_to_write.extend(item)
 
-            if(lista_tema_real):
-                dados.append(lista_tema_real[0])
-                dados.append(lista_tema_real[1])
+            if real_theme_info:
+                data_to_write.extend(real_theme_info)
             else:
-                dados.append("NA")
-                dados.append("NA")
-            with open(file_similarity, mode='a', newline='') as arquivo:
-                writer = csv.writer(arquivo)
-                writer.writerow(dados)
+                data_to_write.extend(["NA", "NA"])
 
-        print(f"Similaridade Computada -dados salvos no arquivo {file_similarity}")
-class EstrategiaCosine(Estrategia):
-    def executar(self, corpus,temas, typeSim,rank,verbose=None):
-        temas_data = pd.DataFrame()
+            with open(file_similarity, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data_to_write)
+
+        print(f"Similarity calculated - data saved to file {file_similarity}")
+
+class CosineSimilarityStrategy(SimilarityStrategy):
+    def execute(self, corpus, themes, similarity_type, rank, verbose=None):
+        themes_data = pd.DataFrame()
         corpus_data = pd.DataFrame()
-        with open(corpus, "rb") as fIn:
-            data = pickle.load(fIn)
-            corpus_data['indice'] = data['indice']
+
+        with open(corpus, "rb") as f:
+            data = pickle.load(f)
+            corpus_data['index'] = data['index']
             corpus_data['topics'] = data['topics']
-            corpus_data['topicsEmbeddings'] = data['topicsEmbeddings'].tolist()
-            corpus_data['numTema'] = data['numTema']
-        with open(temas, "rb") as fIn:
-            data = pickle.load(fIn)
-            #Columns
-            temas_data['indice'] = data['indice']
-            temas_data['sentences'] = data['sentences']
-            temas_data['embeddings'] = data['embeddings'].tolist()
-            temas_data['numTema']= data['numTema']
-        print("Calculando Similaridade por Cosseno")
-        
-        #Salva dados de similaridade em arquivo
-        file_similarity = createFile(corpus,typeSim,rank)
-        #Processamento do texto alvo
-        temas = []
+            corpus_data['topics_embeddings'] = data['topics_embeddings'].tolist()
+            corpus_data['theme_number'] = data['theme_number']
 
-        for indice, linha in corpus_data.iterrows():
-            sys.stdout.write("\r ")  # \r faz o cursor retroceder ao início da linha
-            sys.stdout.write(f' Percentual concluído: {indice/len(corpus_data)*100:.2f}%')
-            sys.stdout.flush()  # Força a impressão imediata
+        with open(themes, "rb") as f:
+            data = pickle.load(f)
+            themes_data['index'] = data['index']
+            themes_data['sentences'] = data['sentences']
+            themes_data['embeddings'] = data['embeddings'].tolist()
+            themes_data['theme_number'] = data['theme_number']
+
+        print("Calculating similarity using Cosine")
+
+        file_similarity = create_file(corpus, similarity_type, rank)
+
+        for idx, row in corpus_data.iterrows():
+            sys.stdout.write("\r ")
+            sys.stdout.write(f' Progress: {idx/len(corpus_data)*100:.2f}%')
+            sys.stdout.flush()
             print(" ", end='\r')
-            dados = []
-            dados.append(indice)
-            #numero do tema cadastrado por um analista
-            try:
-                dados.append(int(linha['numTema']))
-            except Exception as erro:
-                print(f"Erro ao capturar numero de tema cadastrado {indice}")
-                continue
-            #try:
-            ranking , lista_tema_real = calc_similarity_cosine(linha['topicsEmbeddings'], temas_data['embeddings'],temas_data['numTema'], rank, linha['numTema'])
-            ##except Exception as erro:
-                #print(f"Erro calculo similaridade indice {indice}") 
-                #continue
+            data_to_write = [idx, int(row['theme_number'])]
 
-            for i, tupla_num_tema in enumerate(ranking):
-                #captura numero do tema sugerido e valor da similaridade
-                dados.append(tupla_num_tema[0])
-                dados.append(tupla_num_tema[1])
+            ranking, real_theme_info = calculate_cosine_similarity(
+                row['topics_embeddings'],
+                themes_data['embeddings'],
+                themes_data['theme_number'],
+                rank,
+                row['theme_number']
+            )
 
-            if(lista_tema_real):
-                dados.append(lista_tema_real[0])
-                dados.append(lista_tema_real[1])
+            for item in ranking:
+                data_to_write.extend(item)
+
+            if real_theme_info:
+                data_to_write.extend(real_theme_info)
             else:
-                dados.append("NA")
-                dados.append("NA")
-            with open(file_similarity, mode='a', newline='') as arquivo:
-                writer = csv.writer(arquivo)
-                writer.writerow(dados)
+                data_to_write.extend(["NA", "NA"])
 
-        print(f"Similaridade Computada -dados salvos no arquivo {file_similarity}")
+            with open(file_similarity, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(data_to_write)
 
+        print(f"Similarity calculated - data saved to file {file_similarity}")
 
-# Classe que usa uma estratégia
-class Contexto:
-    def __init__(self, estrategia):
-        self.estrategia = estrategia
+class StrategyContext:
+    def __init__(self, strategy):
+        self.strategy = strategy
         
-    def executar_estrategia(self, corpus, temas, typeSim,rank, verbose=None):
-        self.estrategia.executar(corpus, temas, typeSim,rank, verbose)
-
-
+    def execute_strategy(self, corpus, themes, similarity_type, rank, verbose=None):
+        self.strategy.execute(corpus, themes, similarity_type, rank, verbose)
 
 def main(args):
-    print("############### PROGRAMA DE CÁLCULO DE SIMILARIDADE ###############")
-    
-    print("############### Configuração ###############")
-    print(f"Type of similarity : {args.type}")
+    print("############### SIMILARITY CALCULATION PROGRAM ###############")
+    print("############### Configuration ###############")
+    print(f"Similarity type: {args.type}")
 
-    tempo_inicio = time.time()
+    start_time = time.time()
     verbose = args.verbose
 
-    # Cria um contexto com a estratégia adequada
     if args.type == 'B':
-        estrategia = EstrategiaBM25()
+        strategy = BM25Strategy()
     elif args.type == 'C':
-        estrategia = EstrategiaCosine()
+        strategy = CosineSimilarityStrategy()
     else:
-        print(f"Tipo de geração de tópicos não reconhecido: {args.type}")
+        print(f"Unrecognized similarity type: {args.type}")
         return
 
-    contexto = Contexto(estrategia)
-    contexto.executar_estrategia(args.corpus,args.temas, args.type,int(args.rank), verbose)
+    context = StrategyContext(strategy)
+    context.execute_strategy(args.corpus, args.themes, args.type, int(args.rank), verbose)
 
-    print("Salvando log ...")
-    tempo_fim = time.time()
-    tempo_total_segundos = tempo_fim - tempo_inicio
-    minutos, segundos = divmod(int(tempo_total_segundos), 60)
+    print("Saving log...")
+    end_time = time.time()
+    total_seconds = end_time - start_time
+    minutes, seconds = divmod(int(total_seconds), 60)
+    print(f"Execution time: {minutes} minutes and {seconds} seconds")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Calculates  the similarity between texts')
+    parser = argparse.ArgumentParser(description='Calculate the similarity between texts')
     parser.add_argument('corpus', help='Path to corpus file')
-    parser.add_argument('temas', help='Path to tema file')
-    parser.add_argument('rank',help='Size of rank')
-    parser.add_argument('type', choices=['B', 'C'], help='Type of similarity, by cosine or bm25')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Increase the verbosity level')
-
-
+    parser.add_argument('themes', help='Path to themes file')
+    parser.add_argument('rank', help='Size of rank')
+    parser.add_argument('type', choices=['B', 'C'], help='Type of similarity: BM25 (B) or Cosine (C)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increase verbosity')
 
     args = parser.parse_args()
     main(args)
